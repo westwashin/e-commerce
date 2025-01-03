62aed2c66ca85232d39ce9876fa48cebbfbd26ce
westwashin <61764924+westwashin@users.noreply.github.com>
2025-01-03 21:51:12 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 
@@ -14,14 +16,26 @@ export default function Navbar() {
         <header className="mb-8 border-b">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
               <Link href="/">
-                <h1>
-                    hello from us
+                <h1 className="text-4xl font-bold">
+                    Next<span className="text-primary">Commerce</span>
                 </h1>
               </Link>    
 
               <nav className="hidden gap-12 lg:flex 2xl:ml-16">
                 {links.map((link, idx)=>(
-                    <div key={idx}></div>
+                    <div key={idx}>
+                        {pathname === link.href ? (
+                            <Link className="text-lg font-semibold text-primary" href={link.href}>
+                                {link.name}
+                            </Link>
+                        ): (
+                            <Link href={link.href} className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary">
+                                {}
+                            </Link>
+                        )
+                    
+                    }
+                    </div>
                 ))}
               </nav>
             </div>
