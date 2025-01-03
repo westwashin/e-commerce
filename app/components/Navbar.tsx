d461957e5f5d56b9ec97c80b802560b413c9643c
westwashin <61764924+westwashin@users.noreply.github.com>
2025-01-04 05:44:53 +0700

    add stripe payment

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,57 +1,65 @@
 "use client";
 
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {usePathname} from "next/navigation";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ShoppingBag } from "lucide-react";
+import { useShoppingCart } from "use-shopping-cart";
 
 const links = [
-    {name:"Home", href:"/"},
-    {name:"Men", href: "/Men"},
-    {name:"Women", href: "/Women"},
-    {name:"Teens", href: "/Teens"},
+  { name: "Home", href: "/" },
+  { name: "Men", href: "/Men" },
+  { name: "Women", href: "/Women" },
+  { name: "Teens", href: "/Teens" },
 ];
 
 export default function Navbar() {
-    const pathname = usePathname()
-    return (
-        <header className="mb-8 border-b">
-            <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
-              <Link href="/">
-                <h1 className="text-2xl md:text-4xl font-bold">
-                    Toko<span className="text-primary">Lasem</span>
-                </h1>
-              </Link>    
+  const pathname = usePathname();
+  const { handleCartClick } = useShoppingCart();
+  return (
+    <header className="mb-8 border-b">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+        <Link href="/">
+          <h1 className="text-2xl md:text-4xl font-bold">
+            Toko<span className="text-primary">Lasem</span>
+          </h1>
+        </Link>
 
-              <nav className="hidden gap-12 lg:flex 2xl:ml-16">
-                {links.map((link, idx)=>(
-                    <div key={idx}>
-                        {pathname === link.href ? (
-                            <Link className="text-lg font-semibold text-primary" href={link.href}>
-                                {link.name}
-                            </Link>
-                        ): (
-                            <Link href={link.href} className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary">
-                                {link.name}
-                            </Link>
-                        )
-                    
-                    }
-                    </div>
-                ))}
-              </nav>
-              <div className="flex divide-x border-r sm:border-l">
-                <Button
-                variant={"outline"}
-                className="flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 sm:w-20 md:w-24 rounded-none"
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+          {links.map((link, idx) => (
+            <div key={idx}>
+              {pathname === link.href ? (
+                <Link
+                  className="text-lg font-semibold text-primary"
+                  href={link.href}
                 >
-                <ShoppingBag />
-                <span className="hidden text-xs font-semibold text-gray-500 sm:block">
-                    Cart
-                </span>
-                </Button>
-              </div>
+                  {link.name}
+                </Link>
+              ) : (
+                <Link
+                  href={link.href}
+                  className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-primary"
+                >
+                  {link.name}
+                </Link>
+              )}
             </div>
-        </header>
-    )
+          ))}
+        </nav>
+
+        <div className="flex divide-x border-r sm:border-l">
+          <Button
+            variant={"outline"}
+            onClick={() => handleCartClick()}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+          >
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+              Cart
+            </span>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 }
