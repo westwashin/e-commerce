bf3c83f90a38b36bdf9f23b2adbf913df6002b1b
westwashin <61764924+westwashin@users.noreply.github.com>
2025-01-04 04:21:29 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import CartProvider from "./components/Providers";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Newest from "./components/Newest";
+import ShoppingCartModal from "./components/ShoppingCartModal";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,19 +14,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <Hero />
-        <Newest />
-      
-        {children}
+      <body className={inter.className}>
+        <CartProvider>
+          <Navbar />
+          <ShoppingCartModal />
+          {children}
+        </CartProvider>
       </body>
     </html>
   );
