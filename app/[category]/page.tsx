5e18c96ab2aecc5fbd70789fc963f2fd3de98812
westwashin <61764924+westwashin@users.noreply.github.com>
2025-01-05 16:50:47 +0700

    update

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,36 +1,41 @@
-import { Metadata } from 'next';
-import { simplifiedProduct } from '../interface';
-import { client } from '../lib/sanity';
-import Image from 'next/image';
-import Link from 'next/link';
+import Link from "next/link";
+import { simplifiedProduct } from "../interface";
+import { client } from "../lib/sanity";
+import Image from "next/image";
 
-// Helper function for fetching data
-async function getData(category: string): Promise<simplifiedProduct[]> {
-  const query = `*[_type == "product" && category->name == $category] {
-    _id,
-    "imageUrl": images[0].asset->url,
-    price,
-    name,
-    "slug": slug.current,
-    "categoryName": category->name
-  }`;
-  return await client.fetch(query, { category });
+async function getData(cateogry: string) {
+  const query = `*[_type == "product" && category->name == "${cateogry}"] {
+        _id,
+          "imageUrl": images[0].asset->url,
+          price,
+          name,
+          "slug": slug.current,
+          "categoryName": category->name
+      }`;
+
+  const data = await client.fetch(query);
+
+  return data;
 }
 
-// Page component
+export const dynamic = "force-dynamic";
+
 export default async function CategoryPage({
   params,
 }: {
   params: { category: string };
 }) {
-  const data = await getData(params.category);
+  const data: simplifiedProduct[] = await getData(params.category);
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 sm:px-6  lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          Our Products for {params.category}
-        </h2>
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Our Products for {params.category}
+          </h2>
+        </div>
+
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
@@ -43,13 +48,21 @@ export default async function CategoryPage({
                   height={300}
                 />
               </div>
+
               <div className="mt-4 flex justify-between">
-                <h3 className="text-sm text-gray-700">
-                  <Link href={`/product/${product.slug}`}>
-                    {product.name}
-                  </Link>
-                </h3>
-                <p className="text-sm text-gray-900">${product.price}</p>
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <Link href={`/product/${product.slug}`}>
+                      {product.name}
+                    </Link>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">
+                    {product.categoryName}
+                  </p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </p>
               </div>
             </div>
           ))}
